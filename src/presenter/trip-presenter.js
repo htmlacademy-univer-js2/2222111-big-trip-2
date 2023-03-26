d15cdf3a0fc07e69ef56f10f90f1c999673beab6
ilyaO <rikgogs@gmail.com>
2023-03-26 16:03:55 +0500

    шаблонизируй это

--- src/presenter/trip-presenter.js
+++ src/presenter/trip-presenter.js
@@ -1,24 +1,27 @@
 import { render } from '../render.js';
-import CreatingFormView from '../view/creating-form.js';
 import EditingFormView from '../view/editing-form.js';
 import SortingView from '../view/sorting.js';
 import TripListView from '../view/trip-list.js';
 import WayPointView from '../view/way-point.js';
 
-export default class TripPresenter{
-  constructor({container}){
+export default class TripPresenter {
+  constructor(container) {
     this.container = container;
     this.component = new TripListView();
   }
 
-  init(){
+  init(pointsModel) {
+    this.pointsModel = pointsModel;
+    this.boardPoints = [...this.pointsModel.getPoints()];
+    this.destinations = [...this.pointsModel.getDestinations()];
+    this.offers = [...this.pointsModel.getOffers()];
+
     render(new SortingView(), this.container);
     render(this.component, this.container);
-    render(new EditingFormView(), this.component.getElement());
-    render(new CreatingFormView(), this.component.getElement());
-    for (let i = 0; i < 3; i++){
-      render(new WayPointView(), this.component.getElement());
+    render(new EditingFormView(this.boardPoints[0], this.destinations, this.offers), this.component.getElement());
+
+    for (const point of this.boardPoints){
+      render(new WayPointView(point, this.destinations, this.offers), this.component.getElement());
     }
   }
 }
-
