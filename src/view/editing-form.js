11a6c9ab5d1f36ba839cdd233e856ac74e0771c1
ilyaO <rikgogs@gmail.com>
2023-03-28 18:06:46 +0500

    Революция или эволюция(1)

--- src/view/editing-form.js
+++ src/view/editing-form.js
@@ -131,24 +131,30 @@ const createEditingFormTemplate = (point, destinations, offers) => {
 };
 
 export default class EditingFormView {
+
+  #element = null;
+  #point = null;
+  #destination = null;
+  #offers = null;
+
   constructor(point, destination, offers) {
-    this.point = point;
-    this.destination = destination;
-    this.offers = offers;
+    this.#point = point;
+    this.#destination = destination;
+    this.#offers = offers;
   }
 
-  getTemplate () {
-    return createEditingFormTemplate(this.point, this.destination, this.offers);
+  get template () {
+    return createEditingFormTemplate(this.#point, this.#destination, this.#offers);
   }
 
-  getElement() {
-    if (!this.element){
-      this.element = createElement(this.getTemplate());
+  get element() {
+    if (!this.#element){
+      this.#element = createElement(this.template);
     }
-    return this.element;
+    return this.#element;
   }
 
   removeElement() {
-    this.element = null;
+    this.#element = null;
   }
 }
