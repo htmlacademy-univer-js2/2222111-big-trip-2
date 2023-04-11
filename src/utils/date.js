aa7111c7fd6ac31b5301fd21fa2df70723576ba7
ilyaO <rikgogs@gmail.com>
2023-04-11 18:02:13 +0500

    Шаблонизируй то

--- src/utils/date.js
+++ src/utils/date.js
@@ -30,18 +30,12 @@ const getTime = (date) => dayjs(date).format(TIME_FORMAT);
 
 const getDateTime = (date) => dayjs(date).format(DATE_TIME_FORMAT);
 
-const getRandomInteger = (a = 0, b = 1) => {
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
 
-  return Math.floor(lower + Math.random() * (upper - lower + 1));
-};
+const isPointDateInProgress = (dateFrom, dateTo) => dateFrom.isBefore(dayjs()) && dateTo.isAfter(dayjs());
 
-const getRandomElement = (elements) => {
-  const MIN = 0;
-  const max = elements.length - 1;
-  return elements[getRandomInteger(MIN, max)];
-};
+const isPointDateInPast = (dateTo) => dateTo.isBefore(dayjs());
 
-export { getRandomInteger, getRandomElement, humanizePointDueDate, duration, getDate, getDateTime, getTime };
+const isPointDateInFuture = (dateFrom) => dateFrom.isAfter(dayjs());
+
+export {humanizePointDueDate, duration, getDate, getDateTime, getTime, isPointDateInPast, isPointDateInFuture, isPointDateInProgress };
 
