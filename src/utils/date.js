cc1c9a68523f428ea7afeb266c0f5fb5fb90b4d6
ilyaO <rikgogs@gmail.com>
2023-04-25 17:48:18 +0500

    Большие перемены (часть 2)

--- src/utils/date.js
+++ src/utils/date.js
@@ -37,5 +37,16 @@ const isPointDateInPast = (dateTo) => dateTo.isBefore(dayjs());
 
 const isPointDateInFuture = (dateFrom) => dateFrom.isAfter(dayjs());
 
-export {humanizePointDueDate, duration, getDate, getDateTime, getTime, isPointDateInPast, isPointDateInFuture, isPointDateInProgress };
+const sortPricePoint = (pointA, pointB) => pointB.basePrice - pointA.basePrice;
+
+const sortDayPoint = (pointA, pointB) => dayjs(pointA.dateFrom).diff(dayjs(pointB.dateFrom));
+
+const sortTimePoint = (pointA, pointB) => {
+  const timePointA = dayjs(pointA.dateTo).diff(dayjs(pointA.dateFrom));
+  const timePointB = dayjs(pointB.dateTo).diff(dayjs(pointB.dateFrom));
+  return timePointB - timePointA;
+};
+
+export {humanizePointDueDate, duration, getDate, getDateTime, getTime, isPointDateInPast, isPointDateInFuture, isPointDateInProgress,
+  sortPricePoint, sortDayPoint, sortTimePoint};
 
